3887af936bc1e1c8a3d208d818496d75cb0dc487
sophialaurans <sophialaurans@gmail.com>
2024-08-28 01:38:50 -0300

    chore: add user_id to OrderItems table and clear database

--- stockly/backend/app/models.py
+++ stockly/backend/app/models.py
@@ -45,6 +45,7 @@ class Orders(db.Model):
 class OrderItems(db.Model):
     __tablename__ = 'order_items'
     order_item_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id'))
     product_id = db.Column(db.Integer, db.ForeignKey('products.product_id'))
     quantity = db.Column(db.Integer, nullable=False, default=1)
