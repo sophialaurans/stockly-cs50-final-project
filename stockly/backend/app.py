659c07aa4f72b4bb12c57d6a93c96498ff789025
sophialaurans <sophialaurans@gmail.com>
2024-10-30 17:56:05 -0300

    update heroku config

--- stockly/backend/app.py
+++ stockly/backend/app.py
@@ -1,7 +1,9 @@
 from app import create_app
+import os
 
 app = create_app()
 
 # run the application if this file is executed as the main program
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
